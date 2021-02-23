eb52b2d685217ed2b6c37f9cc37160311ecaa67a
Doc <jcbwsloan@gmail.com>
2021-02-22 21:29:21 -0600

    Added in code for those not using the ZDOTDIR variable, so the plugin will use their home directory instead.

--- p10k-promptconfig.plugin.zsh
+++ p10k-promptconfig.plugin.zsh
@@ -1,30 +1,56 @@
 # This file handles P10K themes if $P10K_PROMPT is set in .zshrc
 # To use a certain p10k theme, set $P10K_PROMPT to name of theme in .zshrc
-# To install a new theme, create a file and save it as `THEME_NAME`.zsh in P10K-themes
-if [[ -n $P10K_PROMPT && $ZSH_THEME = "powerlevel10k/powerlevel10k" ]]; then
-    if [ -f "$ZDOTDIR/.$P10K_PROMPT.zsh" ]; then
-        source "$ZDOTDIR/.$P10K_PROMPT.zsh"
-    else
-        echo "$POWERLEVEL10K file not found. Trying to use .p10k.zsh file instead...\n"
-        if [ -f $ZDOTDIR/.p10k.zsh ]; then
-            source $ZDOTDIR/.p10k.zsh
-        else # notify user and ask to run p10k configure to generate .p10k.zsh file.
-            echo ".p10k.zsh file not found."
-            while true; do
-                echo "Run p10k configure to generate one?"
-                echo "[Y/n]: "
-                read -rsk 1 yn
-                case $yn in
-                [Yy]* )
-                    p10k configure
-                    break;;
-                [Nn]* )
-                    echo "Okay, exiting..."
-                    break;;
-                * )
-                    ;;
-                esac
-            done
+# To install a new theme, create a file and save it as `.<theme_name>.zsh` in the directory you keep your .zsh files (either your home directory or your $ZDOTDIR directory)
+if [[ "$ZSH_THEME" = "powerlevel10k/powerlevel10k" ]]; then # plugin is only useful if using powerlevel10k theme
+    if [[ ! -z "$ZDOTDIR" ]]; then # ZDOTDIR variable is set, can use it
+        if [[ -f "$ZDOTDIR/.$P10K_PROMPT.zsh" ]]; then
+            source $ZDOTDIR/.$P10K_PROMPT.zsh
+        else
+            if [[ -f "$ZDOTDIR/.p10k.zsh" ]]; then
+                source $ZDOTDIR/.p10k.zsh
+            else # notify user and ask to run p10k configure to generate .p10k.zsh file.
+                while true; do
+                    echo ".p10k.zsh file not found."
+                    echo "Run p10k configure to generate one?"
+                    echo "[Y/n]: "
+                    read -rsk 1 yn
+                    case $yn in
+                    [Yy]* )
+                        p10k configure
+                        break;;
+                    [Nn]* )
+                        echo "Okay, exiting..."
+                        break;;
+                    * )
+                        ;;
+                    esac
+                done
+            fi
+        fi
+    else # ZDOTDIR is not set, will use user's home directory
+        if [[ -f "~/.$P10K_PROMPT.zsh" ]]; then
+            source ~/.$P10K_PROMPT.zsh
+        else
+            if [[ -f "~/.p10k.zsh" ]]; then
+                source ~/.p10k.zsh
+            else
+                while true; do
+                    echo ".p10k.zsh file not found."
+                    echo "Run p10k configure to generate one?"
+                    echo "[Y/n]: "
+                    read -rsk 1 yn
+                    case $yn in
+                    [Yy]* )
+                        p10k configure
+                        break;;
+                    [Nn]* )
+                        echo "Okay, exiting..."
+                        break;;
+                    * )
+                        ;;
+                    esac
+                done
+            fi
         fi
     fi
 fi
