e2db3e8232f701fe1f3bb1d8fc4803511ca7e918
Doc <jcbwsloan@gmail.com>
2021-03-07 16:46:44 -0600

    Took out the if else statements for those using their home directory and those using ZDOTDIR to set their directory, added variable that will be set to user's home directory if ZDOTDIR isn't set

--- p10k-promptconfig.plugin.zsh
+++ p10k-promptconfig.plugin.zsh
@@ -1,56 +1,34 @@
 # This plugin handles P10K themes if $P10K_PROMPT is set in .zshrc
 # To use a certain p10k theme, set $P10K_PROMPT to name of theme in .zshrc (e.g. P10K_PROMPT="theme" to use the theme file named `.theme.zsh`)
-# To install a new theme, create a file and save it as `.<theme_name>.zsh` in the directory you keep your .zsh files (either your home directory or your $ZDOTDIR directory)
-if [[ "$ZSH_THEME" = "powerlevel10k/powerlevel10k" && -n "$P10K_PROMPT" ]]; then # plugin is only useful if using powerlevel10k theme and if $P10K_PROMPT is set
-    if [[ ! -z "$ZDOTDIR" ]]; then # ZDOTDIR variable is set, can use it
-        if [[ -f "$ZDOTDIR/.$P10K_PROMPT.zsh" ]]; then
-            source $ZDOTDIR/.$P10K_PROMPT.zsh
-        else
-            if [[ -f "$ZDOTDIR/.p10k.zsh" ]]; then
-                source $ZDOTDIR/.p10k.zsh
-            else # notify user and ask to run p10k configure to generate .p10k.zsh file.
-                while true; do
-                    echo ".p10k.zsh file not found."
-                    echo "Run p10k configure to generate one?"
-                    echo "[Y/n]: "
-                    read -rsk 1 yn
-                    case $yn in
-                    [Yy]* )
-                        p10k configure
-                        break;;
-                    [Nn]* )
-                        echo "Okay, exiting..."
-                        break;;
-                    * )
-                        ;;
-                    esac
-                done
-            fi
-        fi
-    else # ZDOTDIR is not set, will use user's home directory
-        if [[ -f "~/.$P10K_PROMPT.zsh" ]]; then
-            source ~/.$P10K_PROMPT.zsh
-        else
-            if [[ -f "~/.p10k.zsh" ]]; then
-                source ~/.p10k.zsh
-            else
-                while true; do
-                    echo ".p10k.zsh file not found."
-                    echo "Run p10k configure to generate one?"
-                    echo "[Y/n]: "
-                    read -rsk 1 yn
-                    case $yn in
-                    [Yy]* )
-                        p10k configure
-                        break;;
-                    [Nn]* )
-                        echo "Okay, exiting..."
-                        break;;
-                    * )
-                        ;;
-                    esac
-                done
-            fi
+# To install a new p10k prompt theme, create a file with p10k theme settings
+# Save it as `.<theme_name>.zsh` in the directory with your .zsh files (most likely the same directory with your .zshrc file)
+
+[ -v ZDOTDIR ] && BASE_DIR="$ZDOTDIR" || BASE_DIR="$HOME"
+
+# plugin is only useful if using powerlevel10k theme and if $P10K_PROMPT is set
+if [[ "$ZSH_THEME" = "powerlevel10k/powerlevel10k" && -n "$P10K_PROMPT" ]]; then
+    if [ -f "$BASE_DIR/.$P10K_PROMPT.zsh" ]; then
+        source $BASE_DIR/.$P10K_PROMPT.zsh
+    else
+        if [ -f "$BASE_DIR/.p10k.zsh" ]; then
+            source $BASE_DIR/.p10k.zsh
+        else # notify user and ask to run p10k configure to generate .p10k.zsh file.
+            while true; do
+                echo ".p10k.zsh file not found."
+                echo "Run p10k configure to generate one?"
+                echo "[Y/n]: "
+                read -rsk 1 yn
+                case $yn in
+                [Yy]* )
+                    p10k configure
+                    break;;
+                [Nn]* )
+                    echo "Okay, exiting..."
+                    break;;
+                * )
+                    ;;
+                esac
+            done
         fi
     fi
 fi
