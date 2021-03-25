2f37dad5543fb07c1c6eff94ad3c35d93e70d55e
Doc <jcbwsloan@gmail.com>
2021-03-25 00:47:03 -0500

    Added more verbose messages to inform when plugin isn't functioning correctly and some other minor changes

--- p10k-promptconfig.plugin.zsh
+++ p10k-promptconfig.plugin.zsh
@@ -1,9 +1,11 @@
-# This plugin handles P10K themes if $P10K_PROMPT is set in .zshrc
-# To use a certain p10k theme, set $P10K_PROMPT to name of theme in .zshrc (e.g. P10K_PROMPT="theme" to use the theme file named `.theme.zsh`)
-# To install a new p10k prompt theme, create a file with p10k theme settings
+#!/bin/zsh
+
+# To use a certain p10k theme, set $P10K_PROMPT to name of theme in .zshrc
+# (e.g. `P10K_PROMPT="theme"` to use the theme file: `.theme.zsh`)
+# To install a new p10k prompt theme, create a file with the theme settings you want
 # Save it as `.<theme_name>.zsh` in the directory with your .zsh files (most likely the same directory with your .zshrc file)
 
-[ -v ZDOTDIR ] && BASE_DIR="$ZDOTDIR" || BASE_DIR="$HOME"
+[ -v ZDOTDIR ] && local BASE_DIR="$ZDOTDIR" || local BASE_DIR="$HOME"
 
 # plugin is only useful if using powerlevel10k theme and if $P10K_PROMPT is set
 if [[ "$ZSH_THEME" = "powerlevel10k/powerlevel10k" && -n "$P10K_PROMPT" ]]; then
@@ -12,10 +14,14 @@ if [[ "$ZSH_THEME" = "powerlevel10k/powerlevel10k" && -n "$P10K_PROMPT" ]]; then
     else
         if [ -f "$BASE_DIR/.p10k.zsh" ]; then
             source $BASE_DIR/.p10k.zsh
+            echo "[p10k-promptconfig]: \".$P10K_PROMPT.zsh\" file not found!"
+            echo "[p10k-promptconfig]: \".p10k.zsh\" file found, will fallback to using it for now"
+            echo "[p10k-promptconfig]: To disable this message, correctly set P10K_PROMPT variable in .zshrc (set P10K_PROMPT=\"p10k\" in .zshrc to use \".p10k.zsh\" without seeing this message)"
         else # notify user and ask to run p10k configure to generate .p10k.zsh file.
             while true; do
-                echo ".p10k.zsh file not found."
-                echo "Run p10k configure to generate one?"
+                echo "[p10k-promptconfig]: \".$P10K_PROMPT.zsh\" file not found!"
+                echo "[p10k-promptconfig]: .p10k.zsh file not found!"
+                echo "[p10k-promptconfig]: Run command p10k configure to generate one?"
                 echo "[Y/n]: "
                 read -rsk 1 yn
                 case $yn in
@@ -23,7 +29,7 @@ if [[ "$ZSH_THEME" = "powerlevel10k/powerlevel10k" && -n "$P10K_PROMPT" ]]; then
                     p10k configure
                     break;;
                 [Nn]* )
-                    echo "Okay, exiting..."
+                    echo "[p10k-promptconfig]: Okay, exiting..."
                     break;;
                 * )
                     ;;
@@ -32,3 +38,5 @@ if [[ "$ZSH_THEME" = "powerlevel10k/powerlevel10k" && -n "$P10K_PROMPT" ]]; then
         fi
     fi
 fi
+
+unset BASE_DIR
